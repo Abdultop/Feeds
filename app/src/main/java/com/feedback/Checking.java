47d4d19b7d819ba4a4551aabd5b6fab33e02fe4f
abdul <abdul>
2017-01-09 17:40:03 +0530

    Commit Changes

--- app/src/main/java/com/feedback/Checking.java
+++ app/src/main/java/com/feedback/Checking.java
@@ -1,10 +1,11 @@
 package com.feedback;
 
+import android.app.Activity;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by abdul on 9/1/17.
  */
 
-public class Checking extends Fragment {
+public class Checking extends Activity {
 }
